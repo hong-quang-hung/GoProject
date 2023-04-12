1cd183d0ea6d6b0e9498374b517e2b8655571e82
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-13 06:23:18 +0700

    Update 71. Simplify Path.go

--- Leetcode/solutions/medium/71. Simplify Path.go
+++ Leetcode/solutions/medium/71. Simplify Path.go
@@ -1,17 +1,35 @@
 package medium
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Reference: https://leetcode.com/problems/simplify-path/
 func Leetcode_Simplify_Path() {
-	fmt.Println("Input: path = '/home/'")
-	fmt.Println("Output:", simplifyPath("/home/"))
-	fmt.Println("Input: path = '/../'")
-	fmt.Println("Output:", simplifyPath("/../"))
-	fmt.Println("Input: path = '/home//foo/'")
-	fmt.Println("Output:", simplifyPath("/home//foo/"))
+	fmt.Println("Input: path = '/.'")
+	fmt.Println("Output:", simplifyPath("/."))
+	fmt.Println("Input: path = '/a/../../b/../c//.//'")
+	fmt.Println("Output:", simplifyPath("/a/../../b/../c//.//"))
+	fmt.Println("Input: path = '/a//b////c/d//././/..'")
+	fmt.Println("Output:", simplifyPath("/a//b////c/d//././/.."))
 }
 
 func simplifyPath(path string) string {
-	return ""
+	contents := strings.Split(path, "/")
+	lenContents := len(contents)
+
+	for i := 0; i < lenContents; i++ {
+		char := contents[i]
+		if (char == ".." && i == 0) || (char == "" && i < lenContents) || char == "." {
+			contents = append(contents[:i], contents[i+1:]...)
+			i--
+			lenContents--
+		} else if char == ".." {
+			contents = append(contents[:i-1], contents[i+1:]...)
+			i = i - 2
+			lenContents = lenContents - 2
+		}
+	}
+	return "/" + strings.Join(contents, "/")
 }
