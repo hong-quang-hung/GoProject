f5d38793ffe55a9498e91357ceadd9adfd67f16f
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-15 19:16:36 +0700

    leetcode || 1572. Matrix Diagonal Sum

--- Leetcode/solutions/easy/1572. Matrix Diagonal Sum.go
+++ Leetcode/solutions/easy/1572. Matrix Diagonal Sum.go
@@ -12,13 +12,18 @@ func Leetcode_Diagonal_Sum() {
 	fmt.Println("Output:", diagonalSum(utils.S2SoSliceInt("[[1,2,3],[4,5,6],[7,8,9]]")))
 	fmt.Println("Input: mat = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]")
 	fmt.Println("Output:", diagonalSum(utils.S2SoSliceInt("[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]")))
+	fmt.Println("Input: mat = [[5]]")
+	fmt.Println("Output:", diagonalSum(utils.S2SoSliceInt("[[5]]")))
 }
 
 func diagonalSum(mat [][]int) int {
-	n := len(mat)
-	move := [][]int{{0, 0}, {0, n - 1}}
-	for _, m := range move {
-		i, j := m[0], m[1]
+	n, res := len(mat), 0
+	for i := 0; i < n; i++ {
+		res += mat[i][i] + mat[n-1-i][i]
 	}
-	return 1
+
+	if n%2 == 1 {
+		res -= mat[n/2][n/2]
+	}
+	return res
 }
