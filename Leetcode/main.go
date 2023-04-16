40deacf03cd64a174859f215be87884c921e8060
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-17 00:37:48 +0700

    leetcode || 1639. Number of Ways to Form a Target String Given a Dictionary

--- Leetcode/main.go
+++ Leetcode/main.go
@@ -13,7 +13,7 @@ var _PROBLEM_TOTAL_ int
 
 func init() {
 	// _PROBLEM_DEBUG_ = 2646
-	_PROBLEM_DEBUG_ = 198
+	_PROBLEM_DEBUG_ = 1639
 	_PROBLEM_TOTAL_ = 2646
 }
 
---
+++ Leetcode/solutions/hard/1639. Number of Ways to Form a Target String Given a Dictionary.go
@@ -0,0 +1,58 @@
+package hard
+
+import (
+	"fmt"
+)
+
+// Reference: https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/
+func Leetcode_Num_Ways() {
+	fmt.Println("Input: words = ['acca','bbbb','caca'], target = 'aba'")
+	fmt.Println("Output:", numWays([]string{"acca", "bbbb", "caca"}, "aba"))
+	fmt.Println("Input: words = ['abba','baab'], target = 'bab'")
+	fmt.Println("Output:", numWays([]string{"abba", "baab"}, "bab"))
+}
+
+func numWays(words []string, target string) int {
+	n, m := len(target), len(words[0])
+	dp := make([][]int64, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = make([]int64, m+1)
+		for j := 0; j < m+1; j++ {
+			dp[i][j] = -1
+		}
+	}
+
+	cnt := make([][]int, 26)
+	for i := 0; i < 26; i++ {
+		cnt[i] = make([]int, m)
+	}
+
+	for j := 0; j < m; j++ {
+		for _, word := range words {
+			cnt[word[j]-'a'][j]++
+		}
+	}
+	return int(numWaysSolved(dp, cnt, target, n, m))
+}
+
+func numWaysSolved(dp [][]int64, cnt [][]int, target string, i int, j int) int64 {
+	if j == 0 {
+		if i == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if dp[i][j] != -1 {
+		return dp[i][j]
+	}
+
+	dp[i][j] = numWaysSolved(dp, cnt, target, i, j-1)
+	if i > 0 {
+		dp[i][j] += int64(cnt[target[i-1]-'a'][j-1]) * int64(numWaysSolved(dp, cnt, target, i-1, j-1))
+	}
+
+	dp[i][j] %= 1000000007
+	return dp[i][j]
+}
