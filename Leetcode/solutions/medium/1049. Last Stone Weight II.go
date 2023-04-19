a05035e2f625d2e2b826095c1f5d61cb8501c1f5
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-20 03:39:26 +0700

    1049. Last Stone Weight II

--- Leetcode/solutions/medium/1049. Last Stone Weight II.go
+++ Leetcode/solutions/medium/1049. Last Stone Weight II.go
@@ -2,6 +2,7 @@ package medium
 
 import (
 	"fmt"
+	"math"
 )
 
 // Reference: https://leetcode.com/problems/last-stone-weight-ii/
@@ -13,10 +14,34 @@ func Leetcode_Last_Stone_Weight_II() {
 }
 
 func lastStoneWeightII(stones []int) int {
-	dp := make([]int, len(stones))
-	return lastStoneWeightIISolve(stones, dp, 0)
+	sum, n := 0, len(stones)
+	for _, stone := range stones {
+		sum += stone
+	}
+
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, sum)
+		for j := 0; j < sum; j++ {
+			dp[i][j] = -1
+		}
+	}
+	return lastStoneWeightIISolved(stones, dp, 0, 0, 0)
 }
 
-func lastStoneWeightIISolve(stones []int, dp []int, i int) int {
-	return 0
+func lastStoneWeightIISolved(stones []int, dp [][]int, i int, sumL int, sumR int) int {
+	if i == len(stones) {
+		return int(math.Abs(float64(sumL) - float64(sumR)))
+	}
+
+	if dp[i][sumL] != -1 {
+		return dp[i][sumL]
+	}
+
+	if dp[i][sumR] != -1 {
+		return dp[i][sumR]
+	}
+
+	dp[i][sumL] = min(lastStoneWeightIISolved(stones, dp, i+1, sumL+stones[i], sumR), lastStoneWeightIISolved(stones, dp, i+1, sumL, sumR+stones[i]))
+	return dp[i][sumL]
 }
