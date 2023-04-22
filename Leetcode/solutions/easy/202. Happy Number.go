de73c704f003d852f35b20d4215dbc7e26d5b722
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-23 00:15:50 +0700

    202. Happy Number

---
+++ Leetcode/solutions/easy/202. Happy Number.go
@@ -0,0 +1,34 @@
+package easy
+
+import (
+	"fmt"
+	"math"
+)
+
+// Reference: https://leetcode.com/problems/happy-number/
+func Leetcode_Is_Happy() {
+	fmt.Println("Input: num = 19")
+	fmt.Println("Output:", isHappy(19))
+	fmt.Println("Input: num = 2")
+	fmt.Println("Output:", isHappy(2))
+	fmt.Println("Input: num = 3")
+	fmt.Println("Output:", isHappy(3))
+}
+
+func isHappy(n int) bool {
+	m := make(map[int]bool)
+	for n != 1 {
+		if m[n] {
+			return false
+		} else {
+			m[n] = true
+		}
+
+		sum := 0
+		for _, number := range fmt.Sprint(n) {
+			sum += int(math.Pow(float64(number-'0'), 2))
+		}
+		n = sum
+	}
+	return true
+}
