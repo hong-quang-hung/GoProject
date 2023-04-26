c8b76e0c63f8408d07acf05e15e1a32d0e4d6154
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-26 08:28:38 +0700

    1582. Special Positions in a Binary Matrix #2

--- Leetcode/solutions/easy/1582. Special Positions in a Binary Matrix.go
+++ Leetcode/solutions/easy/1582. Special Positions in a Binary Matrix.go
@@ -8,18 +8,18 @@ import (
 
 // Reference: https://leetcode.com/problems/special-positions-in-a-binary-matrix/
 func Leetcode_Num_Special() {
-	// fmt.Println("Input: mat = [[1,0,0],[0,0,1],[1,0,0]]")
-	// fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[1,0,0],[0,0,1],[1,0,0]]")))
-	// fmt.Println("Input: mat = [[1,0,0],[0,1,0],[0,0,1]]")
-	// fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[1,0,0],[0,1,0],[0,0,1]]")))
-	// fmt.Println("Input: mat = [[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,1],[0,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0],[0,0,1,1,0,0,0,0]]")
-	// fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,1],[0,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0],[0,0,1,1,0,0,0,0]]")))
+	fmt.Println("Input: mat = [[1,0,0],[0,0,1],[1,0,0]]")
+	fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[1,0,0],[0,0,1],[1,0,0]]")))
+	fmt.Println("Input: mat = [[1,0,0],[0,1,0],[0,0,1]]")
+	fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[1,0,0],[0,1,0],[0,0,1]]")))
+	fmt.Println("Input: mat = [[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,1],[0,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0],[0,0,1,1,0,0,0,0]]")
+	fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,1],[0,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0],[0,0,1,1,0,0,0,0]]")))
 	fmt.Println("Input: mat = [[0,0,0,0,0,0,0,0],[0,0,0,1,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,1,0],[0,1,0,0,0,0,1,0],[0,1,0,0,0,0,0,0]]")
 	fmt.Println("Output:", numSpecial(utils.S2SoSliceInt("[[0,0,0,0,0,0,0,0],[0,0,0,1,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,1,0],[0,1,0,0,0,0,1,0],[0,1,0,0,0,0,0,0]]")))
 }
 
 func numSpecial(mat [][]int) int {
-	res, n, m := 0, len(mat), len(mat[0])
+	n, m := len(mat), len(mat[0])
 	r := make([]int, n)
 	c := make([]int, m)
 	for i := 0; i < n; i++ {
@@ -27,15 +27,15 @@ func numSpecial(mat [][]int) int {
 			if mat[i][j] == 1 {
 				r[i]++
 				c[j]++
+			}
+		}
+	}
+
+	res := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if mat[i][j] == 1 && r[i] == 1 && c[j] == 1 {
 				res++
-
-				if r[i] >= 2 {
-					res--
-				}
-
-				if c[j] >= 2 {
-					res--
-				}
 			}
 		}
 	}
