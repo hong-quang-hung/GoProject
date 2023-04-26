3902408f8b9d3e700b407a4ec37a6ad37a6d015a
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-26 08:14:13 +0700

    258. Add Digits #1

---
+++ Leetcode/solutions/easy/258. Add Digits.go
@@ -0,0 +1,25 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/add-digits/
+func Leetcode_Add_Digits() {
+	fmt.Println("Input: num = 38")
+	fmt.Println("Output:", addDigits(38))
+	fmt.Println("Input: num = 5")
+	fmt.Println("Output:", addDigits(5))
+	fmt.Println("Input: num = 38")
+	fmt.Println("Output:", addDigits(578))
+}
+
+func addDigits(num int) int {
+	for num >= 10 {
+		sum := 0
+		for num > 0 {
+			sum += (num % 10)
+			num /= 10
+		}
+		num = sum
+	}
+	return num
+}
