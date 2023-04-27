39f79fc22130d33ecec4a518c2a9549730db7dbd
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-04-27 11:05:10 +0700

    263. Ugly Number

---
+++ Leetcode/solutions/easy/263. Ugly Number.go
@@ -0,0 +1,36 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/ugly-number/
+func Leetcode_Is_Ugly() {
+	fmt.Println("Input: n = 6")
+	fmt.Println("Output:", isUgly(6))
+	fmt.Println("Input: n = 14")
+	fmt.Println("Output:", isUgly(14))
+	fmt.Println("Input: n = 40")
+	fmt.Println("Output:", isUgly(40))
+}
+
+func isUgly(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	if n == 1 || n == 2 || n == 3 || n == 5 {
+		return true
+	}
+
+	if n%2 == 0 {
+		return isUgly(n / 2)
+	}
+
+	if n%3 == 0 {
+		return isUgly(n / 3)
+	}
+
+	if n%5 == 0 {
+		return isUgly(n / 5)
+	}
+	return false
+}
