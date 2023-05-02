6805c63f46512c0199da581f3eb1c70b72b0a5e9
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-02 10:34:38 +0700

    17. Letter Combinations of a Phone Number

---
+++ Leetcode/solutions/medium/17. Letter Combinations of a Phone Number.go
@@ -0,0 +1,35 @@
+package medium
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+func Leetcode_Letter_Combinations() {
+	fmt.Println("Input: digits = '23'")
+	fmt.Println("Output:", letterCombinations("23"))
+	fmt.Println("Input: digits = ''")
+	fmt.Println("Output:", letterCombinations(""))
+	fmt.Println("Input: digits = '2'")
+	fmt.Println("Output:", letterCombinations("2"))
+}
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return nil
+	}
+
+	m := map[byte]string{'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+	res := make([]string, 0)
+	letterCombinationsSolved(digits, m, &res, 0, "")
+	return res
+}
+
+func letterCombinationsSolved(digits string, m map[byte]string, res *[]string, i int, s string) {
+	if i >= len(digits) {
+		*res = append(*res, s)
+		return
+	}
+
+	for _, ch := range m[digits[i]] {
+		letterCombinationsSolved(digits, m, res, i+1, s+string(ch))
+	}
+}
