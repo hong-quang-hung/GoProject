0f6f8db38c02fd2141fb4416302f5469ef304516
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-05 11:41:31 +0700

    2662. Minimum Cost of a Path With Special Roads

--- Leetcode/solutions/medium/2662. Minimum Cost of a Path With Special Roads.go
+++ Leetcode/solutions/medium/2662. Minimum Cost of a Path With Special Roads.go
@@ -1,6 +1,7 @@
 package medium
 
 import (
+	"container/heap"
 	"fmt"
 
 	"leetcode.com/Leetcode/utils"
@@ -14,40 +15,44 @@ func Leetcode_Minimum_Cost() {
 	fmt.Println("Output:", minimumCost([]int{3, 2}, []int{5, 7}, utils.S2SoSliceInt("[[3,2,3,4,4],[3,3,5,5,5],[3,4,5,6,6]]")))
 }
 
+type minimumCostHeap [][]int
+
+func (h minimumCostHeap) Less(i, j int) bool { return h[i][0] < h[j][0] }
+func (h minimumCostHeap) Len() int           { return len(h) }
+func (h minimumCostHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h minimumCostHeap) Empty() bool        { return len(h) == 0 }
+func (h *minimumCostHeap) Pop() interface{} {
+	r := (*h)[(*h).Len()-1]
+	*h = (*h)[0 : (*h).Len()-1]
+	return r
+}
+func (h *minimumCostHeap) Push(i interface{}) {
+	*h = append(*h, i.([]int))
+}
+
 func minimumCost(start []int, target []int, specialRoads [][]int) int {
-	dp := make([][]int, target[0]+1)
-	for i := start[0]; i < target[0]+1; i++ {
-		dp[i] = make([]int, target[1]+1)
-		for j := start[1]; j < target[1]+1; j++ {
-			dp[i][j] = -1
+	m, h := make(map[[2]int]int), new(minimumCostHeap)
+	res := target[0] - start[0] + target[1] - start[1]
+	heap.Push(h, []int{0, start[0], start[1]})
+	for h.Len() > 0 {
+		elem := heap.Pop(h).([]int)
+		curDist, x, y := elem[0], elem[1], elem[2]
+		for _, road := range specialRoads {
+			pair := [2]int{road[2], road[3]}
+			if _, ok := m[pair]; !ok {
+				m[pair] = road[2] - start[0] + road[3] - start[1]
+			}
+
+			d := abs(road[0]-x) + abs(road[1]-y) + curDist + road[4]
+			if m[pair] > d {
+				m[pair] = d
+				heap.Push(h, []int{m[pair], road[2], road[3]})
+			}
 		}
 	}
 
-	m := make([][][]int, target[0]+1)
-	for i := start[0]; i < target[0]+1; i++ {
-		m[i] = make([][]int, target[1]+1)
+	for _, road := range specialRoads {
+		res = min(res, m[[2]int{road[2], road[3]}]+target[0]-road[2]+target[1]-road[3])
 	}
-
-	for i := 0; i < len(specialRoads); i++ {
-		m[specialRoads[i][2]][specialRoads[i][3]] = []int{specialRoads[i][0], specialRoads[i][1], specialRoads[i][4]}
-	}
-	return minimumCostSolved(start, target, m, dp, target[0], target[1])
-}
-
-func minimumCostSolved(start []int, target []int, m [][][]int, dp [][]int, i int, j int) int {
-	if i < start[0] || j < start[1] {
-		return 0
-	}
-
-	if dp[i][j] != -1 {
-		return dp[i][j]
-	}
-
-	startInfo := m[i][j]
-	if startInfo != nil && startInfo[0] != 0 && startInfo[1] != 0 {
-		dp[i][j] = min(min(minimumCostSolved(start, target, m, dp, i-1, j)+1, minimumCostSolved(start, target, m, dp, i, j-1)+1), minimumCostSolved(start, target, m, dp, startInfo[0], startInfo[1])+startInfo[2])
-	} else {
-		dp[i][j] = min(minimumCostSolved(start, target, m, dp, i-1, j)+1, minimumCostSolved(start, target, m, dp, i, j-1)+1)
-	}
-	return dp[i][j]
+	return res
 }
