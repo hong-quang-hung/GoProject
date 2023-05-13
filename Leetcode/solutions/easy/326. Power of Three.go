6003f9a2702ef5509bfa9cdf56b209f22dc1b0fe
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-13 11:16:46 +0700

    326. Power of Three

---
+++ Leetcode/solutions/easy/326. Power of Three.go
@@ -0,0 +1,22 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/power-of-three/
+func Leetcode_Is_Power_Of_Three() {
+	fmt.Println("Input: n = 27")
+	fmt.Println("Output:", isPowerOfThree(27))
+	fmt.Println("Input: n = -1")
+	fmt.Println("Output:", isPowerOfThree(-1))
+}
+
+func isPowerOfThree(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	for n%3 == 0 {
+		n /= 3
+	}
+	return n == 1
+}
