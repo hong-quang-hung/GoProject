c26814485eb892c7b74784ac344d2ec501b95303
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-15 14:55:56 +0700

    39. Combination Sum

--- Leetcode/solutions/medium/39. Combination Sum.go
+++ Leetcode/solutions/medium/39. Combination Sum.go
@@ -1,15 +1,37 @@
 package medium
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// Reference: https://leetcode.com/problems/generate-parentheses/
+// Reference: https://leetcode.com/problems/combination-sum/
 func Leetcode_Combination_Sum() {
 	fmt.Println("Input: candidates = [2,3,6,7], target = 7")
 	fmt.Println("Output:", combinationSum([]int{2, 3, 6, 7}, 7))
 	fmt.Println("Input: candidates = [2,3,5], target = 8")
 	fmt.Println("Output:", combinationSum([]int{2, 3, 5}, 8))
+	fmt.Println("Input: candidates = [8,7,4,3], target = 11")
+	fmt.Println("Output:", combinationSum([]int{8, 7, 4, 3}, 11))
 }
 
 func combinationSum(candidates []int, target int) [][]int {
-	return nil
+	var res [][]int
+	combinationSumDFS(&res, candidates, target, []int{}, 0)
+	return res
+}
+
+func combinationSumDFS(res *[][]int, candidates []int, target int, current []int, i int) {
+	if target == 0 {
+		*res = append(*res, current)
+	} else {
+		for j := i; j < len(candidates); j++ {
+			if candidates[j] <= target {
+				temp := make([]int, len(current))
+				copy(temp, current)
+				current = append(current, candidates[j])
+				combinationSumDFS(res, candidates, target-candidates[j], current, j)
+				current = temp
+			}
+		}
+	}
 }
