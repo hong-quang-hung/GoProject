f2851cb3b8dd3522e98b2b68d3d680197fdc346b
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-17 08:36:31 +0700

    Update 2680. Maximum OR.go

--- Leetcode/solutions/medium/2680. Maximum OR.go
+++ Leetcode/solutions/medium/2680. Maximum OR.go
@@ -12,37 +12,24 @@ func Leetcode_Maximum_Or() {
 	fmt.Println("Output:", maximumOr([]int{8, 1, 2}, 2))
 	fmt.Println("Input: nums = [10,8,4], k = 1")
 	fmt.Println("Output:", maximumOr([]int{10, 8, 4}, 1))
-	fmt.Println("Input: nums = [6,9,8], k = 1")
-	fmt.Println("Output:", maximumOr([]int{6, 9, 8}, 1))
 }
 
 func maximumOr(nums []int, k int) int64 {
-	n := len(nums)
-	dp := make([][]int64, n)
+	res, n := int64(0), len(nums)
+	pre, suf := make([]int64, n+1), make([]int64, n+1)
+	pre[0] = 0
+	suf[n] = 0
 	for i := 0; i < n; i++ {
-		dp[i] = make([]int64, k+1)
-		for j := 0; j <= k; j++ {
-			dp[i][j] = -1
-		}
-	}
-	return maximumOrSolved(nums, dp, 0, k)
-}
-
-func maximumOrSolved(nums []int, dp [][]int64, i, k int) int64 {
-	if i == len(nums) {
-		return 0
+		pre[i+1] = pre[i] | int64(nums[i])
 	}
 
-	if dp[i][k] != -1 {
-		return dp[i][k]
+	for i := n - 1; i >= 0; i-- {
+		suf[i] = suf[i+1] | int64(nums[i])
 	}
 
-	maxOr := int64(nums[i]) | maximumOrSolved(nums, dp, i+1, k)
-	for j := 1; j <= k; j++ {
-		sum := int64(nums[i]) << j
-		maxOr = max64(maxOr, sum|maximumOrSolved(nums, dp, i+1, k-j))
+	p := int64(1) << k
+	for i := 0; i < n; i++ {
+		res = max64(res, pre[i]|(int64(nums[i])*p)|suf[i+1])
 	}
-
-	dp[i][k] = maxOr
-	return dp[i][k]
+	return res
 }
