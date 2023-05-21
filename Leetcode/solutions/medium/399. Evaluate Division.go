821363d1a5492f040f66e8e63452a2667c0ce773
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-21 09:18:42 +0700

    Update 399. Evaluate Division.go

--- Leetcode/solutions/medium/399. Evaluate Division.go
+++ Leetcode/solutions/medium/399. Evaluate Division.go
@@ -13,5 +13,55 @@ func Leetcode_Calc_Equation() {
 }
 
 func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
-	return nil
+	g := make(map[string]map[string]float64)
+	for i, v := range equations {
+		if g[v[0]] == nil {
+			g[v[0]] = make(map[string]float64)
+		}
+		if g[v[1]] == nil {
+			g[v[1]] = make(map[string]float64)
+		}
+		g[v[0]][v[1]] = values[i]
+		g[v[1]][v[0]] = 1 / values[i]
+	}
+
+	results := make([]float64, 0, len(queries))
+	for _, v := range queries {
+		x := v[0]
+		y := v[1]
+		v := make(map[string]bool)
+		result := calcEquationDFS(x, y, v, g)
+		results = append(results, result)
+	}
+	return results
+}
+
+func calcEquationDFS(x, y string, visited map[string]bool, g map[string]map[string]float64) float64 {
+	_, okX := g[x]
+	_, okY := g[y]
+	if !okX || !okY {
+		return -1.0
+	}
+
+	if len(g[x]) == 0 {
+		return -1.0
+	}
+
+	val, okResult := g[x][y]
+	if okResult {
+		return val
+	}
+
+	for k, _ := range g[x] {
+		if !visited[k] {
+			visited[k] = true
+			tmp := calcEquationDFS(k, y, visited, g)
+			if tmp == -1.0 {
+				continue
+			} else {
+				return tmp * g[x][k]
+			}
+		}
+	}
+	return -1.0
 }
