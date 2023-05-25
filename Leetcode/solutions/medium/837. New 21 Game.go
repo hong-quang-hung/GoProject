3d6974463098e32c56f09057d2d1578dcbb2ca2b
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-05-26 06:39:13 +0700

    837. New 21 Game

--- Leetcode/solutions/medium/837. New 21 Game.go
+++ Leetcode/solutions/medium/837. New 21 Game.go
@@ -13,5 +13,25 @@ func Leetcode_New_21_Game() {
 }
 
 func new21Game(n int, k int, maxPts int) float64 {
-	return 0
+	dp, s := make([]float64, n+1), float64(1)
+	dp[0] = 1
+	if k <= 0 {
+		s = 0
+	}
+
+	for i := 1; i <= n; i++ {
+		dp[i] = s / float64(maxPts)
+		if i < k {
+			s += dp[i]
+		}
+		if i-maxPts >= 0 && i-maxPts < k {
+			s -= dp[i-maxPts]
+		}
+	}
+
+	ans := float64(0)
+	for i := k; i <= n; i++ {
+		ans += dp[i]
+	}
+	return ans
 }
