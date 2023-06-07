23cb4ebe38101b0db5ef4908e8f7e3e1bd4e5171
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-06-07 10:55:52 +0700

    Update 1143. Longest Common Subsequence.go

--- Leetcode/solutions/medium/1143. Longest Common Subsequence.go
+++ Leetcode/solutions/medium/1143. Longest Common Subsequence.go
@@ -19,11 +19,9 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 		dp[i] = make([]int, n+1)
 	}
 
-	for i := 0; i <= m; i++ {
-		for j := 0; j <= n; j++ {
-			if i == 0 || j == 0 {
-				dp[i][j] = 0
-			} else if text1[i-1] == text2[j-1] {
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
 			} else {
 				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
