3e9396bd46cc103ab87d1a2fd6edd11a5c8332c6
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-06-12 00:48:29 +0700

    1146. Snapshot Array

--- Leetcode/solutions/debug.go
+++ Leetcode/solutions/debug.go
@@ -222,6 +222,7 @@ func init() {
 	_SOLUTIONS_[101129] = medium.Leetcode_Shortest_Alternating_Paths
 	_SOLUTIONS_[101140] = medium.Leetcode_Stone_Game_II
 	_SOLUTIONS_[101143] = medium.Leetcode_Longest_Common_Subsequence
+	_SOLUTIONS_[101146] = medium.Leetcode_Snapshot_Array
 	_SOLUTIONS_[101162] = medium.Leetcode_Max_Distance
 	_SOLUTIONS_[101201] = medium.Leetcode_Is_Ugly_III
 	_SOLUTIONS_[101202] = medium.Leetcode_Smallest_String_With_Swaps
---
+++ Leetcode/solutions/medium/1146. Snapshot Array.go
@@ -0,0 +1,56 @@
+package medium
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Reference: https://leetcode.com/problems/snapshot-array/
+func Leetcode_Snapshot_Array() {
+	fmt.Println("Input:")
+	fmt.Println("['SnapshotArray','set','snap','set','get']")
+	fmt.Println("Output:")
+
+	snapshotArr := SnapshotArrayConstructor(3)
+	snapshotArr.Set(0, 5)
+	fmt.Println("snapshotArr.snap()", "=>", snapshotArr.Snap())
+	snapshotArr.Set(0, 6)
+	fmt.Println("snapshotArr.get(0, 0)", "=>", snapshotArr.Get(0, 0))
+}
+
+type SnapshotArray struct {
+	arr     [][][2]int
+	snap_id int
+}
+
+func SnapshotArrayConstructor(length int) SnapshotArray {
+	snap := new(SnapshotArray)
+	snap.arr = make([][][2]int, length)
+	for i := 0; i < length; i++ {
+		snap.arr[i] = make([][2]int, 1)
+		snap.arr[i][0] = [2]int{0, 0}
+	}
+	return *snap
+}
+
+func (s *SnapshotArray) Set(index int, val int) {
+	h := s.arr[index]
+	if h[len(h)-1][0] < s.snap_id && h[len(h)-1][1] != val {
+		s.arr[index] = append(h, [2]int{s.snap_id, val})
+	} else {
+		s.arr[index][len(h)-1][1] = val
+	}
+}
+
+func (s *SnapshotArray) Snap() int {
+	s.snap_id++
+	return s.snap_id - 1
+}
+
+func (s *SnapshotArray) Get(index int, snap_id int) int {
+	h := s.arr[index]
+	i := sort.Search(len(h), func(j int) bool {
+		return (j < len(h)-1 && h[j+1][0] > snap_id) || j == len(h)-1
+	})
+	return h[i][1]
+}
