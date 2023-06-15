126535330e984fc34fa330175092af0dcc3abc2d
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-06-15 16:19:41 +0700

    88. Merge Sorted Array

---
+++ Leetcode/solutions/easy/88. Merge Sorted Array.go
@@ -0,0 +1,21 @@
+package easy
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Reference: https://leetcode.com/problems/merge-sorted-array/
+func Leetcode_Merge_Sorted_Array() {
+	fmt.Println("Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3")
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	merge(nums1, 3, []int{2, 5, 6}, 3)
+	fmt.Println("Output:", nums1)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for i := 0; i < n && i+m < len(nums1); i++ {
+		nums1[m+i] = nums2[i]
+	}
+	sort.Ints(nums1)
+}
