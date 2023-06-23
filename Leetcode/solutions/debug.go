488125f723d48f1dd7d86cb412e3e6781826267a
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-06-23 14:49:37 +0700

    933. Number of Recent Calls

--- Leetcode/solutions/debug.go
+++ Leetcode/solutions/debug.go
@@ -68,6 +68,7 @@ func init() {
 	_SOLUTIONS_[100783] = easy.Leetcode_Min_Diff_In_BST
 	_SOLUTIONS_[100925] = easy.Leetcode_Is_Long_PressedName
 	_SOLUTIONS_[100929] = easy.Leetcode_Num_Unique_Emails
+	_SOLUTIONS_[100933] = easy.Leetcode_Number_Of_Recent_Calls
 	_SOLUTIONS_[100953] = easy.Leetcode_Is_Alien_Sorted
 	_SOLUTIONS_[100989] = easy.Leetcode_Add_To_Array_Form
 	_SOLUTIONS_[101046] = easy.Leetcode_Last_Stone_Weight
---
+++ Leetcode/solutions/easy/933. Number of Recent Calls.go
@@ -0,0 +1,36 @@
+package easy
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Reference: https://leetcode.com/problems/number-of-recent-calls/
+func Leetcode_Number_Of_Recent_Calls() {
+	fmt.Println("Input:")
+	fmt.Println("['RecentCounter', 'ping', 'ping', 'ping', 'ping']")
+	fmt.Println("[[], [1], [100], [3001], [3002]]")
+	fmt.Println("Output:")
+
+	recentCounter := RecentCounterConstructor()
+	fmt.Println("recentCounter.ping(1)", "-->", recentCounter.Ping(1))
+	fmt.Println("recentCounter.ping(100)", "-->", recentCounter.Ping(100))
+	fmt.Println("recentCounter.ping(3001)", "-->", recentCounter.Ping(3001))
+	fmt.Println("recentCounter.ping(3002)", "-->", recentCounter.Ping(3002))
+}
+
+type RecentCounter struct {
+	arr []int
+}
+
+func RecentCounterConstructor() RecentCounter {
+	return RecentCounter{arr: []int{}}
+}
+
+func (rc *RecentCounter) Ping(t int) int {
+	rc.arr = append(rc.arr, t)
+	if t > 3000 {
+		rc.arr = rc.arr[sort.SearchInts(rc.arr, t-3000):]
+	}
+	return len(rc.arr)
+}
