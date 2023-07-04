b55b0cb37c8ad6610e5a2cecc3e169926c9adaf1
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-07-04 08:12:58 +0700

    859. Buddy Strings

--- Leetcode/solutions/easy/859. Buddy Strings.go
+++ Leetcode/solutions/easy/859. Buddy Strings.go
@@ -12,20 +12,36 @@ func Leetcode_Buddy_Strings() {
 	fmt.Println("Output:", buddyStrings("aa", "aa"))
 	fmt.Println("Input: s = 'abcaa', goal = 'abcbb'")
 	fmt.Println("Output:", buddyStrings("abcd", "abcbb"))
-	fmt.Println("Input: s = 'abcd', goal = 'cdab'")
-	fmt.Println("Output:", buddyStrings("abcd", "cdab"))
 }
 
 func buddyStrings(s string, goal string) bool {
-	m1 := [26]int{}
-	m2 := [26]int{}
+	n := len(s)
+	if n != len(goal) {
+		return false
+	}
+
+	hash := make(map[byte]int)
 	count := 0
-	for i := 0; i < len(s); i++ {
-		m1[int(s[i]-'a')]++
-		m2[int(goal[i]-'a')]++
+	for i := 0; i < n; i++ {
 		if s[i] != goal[i] {
 			count++
+			if count > 2 {
+				return false
+			}
+		}
+
+		hash[s[i]]++
+		hash[goal[i]]--
+	}
+
+	if s == goal && len(hash) == n {
+		return false
+	}
+
+	for _, v := range hash {
+		if v != 0 {
+			return false
 		}
 	}
-	return m1 == m2 && count == 2
+	return true
 }
