0ea9fc72dc93c8e73c31afdfa5bf1ad7b16751d7
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-07-10 16:11:20 +0700

    700. Search in a Binary Search Tree

---
+++ Leetcode/solutions/easy/700. Search in a Binary Search Tree.go
@@ -0,0 +1,27 @@
+package easy
+
+import "fmt"
+
+func init() {
+	Solutions[700] = Leetcode_Search_BST
+}
+
+// Reference: https://leetcode.com/problems/search-in-a-binary-search-tree/
+func Leetcode_Search_BST() {
+	fmt.Println("Input: root = [4,2,7,1,3], val = 2")
+	fmt.Println("Output:", STreeNode(searchBST(S2TreeNode("[4,2,7,1,3]"), 2)))
+	fmt.Println("Input: root = [4,2,7,1,3], val = 5")
+	fmt.Println("Output:", STreeNode(searchBST(S2TreeNode("[4,2,7,1,3]"), 5)))
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+
+	return searchBST(root.Right, val)
+}
