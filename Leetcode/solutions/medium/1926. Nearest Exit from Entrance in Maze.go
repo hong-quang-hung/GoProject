9370481e755ea2aae637c30e949fab3715898430
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-07-26 08:27:34 +0700

    1926. Nearest Exit from Entrance in Maze

--- Leetcode/solutions/medium/1926. Nearest Exit from Entrance in Maze.go
+++ Leetcode/solutions/medium/1926. Nearest Exit from Entrance in Maze.go
@@ -11,24 +11,37 @@ func init() {
 		fmt.Println("Output:", nearestExit([][]byte{{'+', '+', '+'}, {'.', '.', '.'}, {'+', '+', '+'}}, []int{1, 0}))
 		fmt.Println("Input: maze = [['.','+']], entrance = [0,0]")
 		fmt.Println("Output:", nearestExit([][]byte{{'.', '+'}}, []int{0, 0}))
+		fmt.Println("Input: maze = [{'+', '.', '+', '+', '+', '+', '+'}, {'+', '.', '+', '.', '.', '.', '+'}, {'+', '.', '+', '.', '+', '.', '+'}, {'+', '.', '.', '.', '+', '.', '+'}, {'+', '+', '+', '+', '+', '.', '+'}], entrance = [3,2]")
+		fmt.Println("Output:", nearestExit([][]byte{{'+', '.', '+', '+', '+', '+', '+'}, {'+', '.', '+', '.', '.', '.', '+'}, {'+', '.', '+', '.', '+', '.', '+'}, {'+', '.', '.', '.', '+', '.', '+'}, {'+', '+', '+', '+', '+', '.', '+'}}, []int{3, 2}))
 	}
 }
 
 func nearestExit(maze [][]byte, entrance []int) int {
 	n, m := len(maze), len(maze[0])
+	visited := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		visited[i] = make([]bool, m)
+	}
+
 	res := 0
 	queue := [][]int{{entrance[0], entrance[1]}}
+	directs := [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
 	for len(queue) > 0 {
 		temp := [][]int{}
+		res++
 		for _, next := range queue {
-			for _, dir := range [][]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}} {
+			if visited[next[0]][next[1]] {
+				continue
+			}
+
+			visited[next[0]][next[1]] = true
+			for _, dir := range directs {
 				row, col := next[0]+dir[0], next[1]+dir[1]
-				if row >= 0 && row < n && col >= 0 && col < m && maze[row][col] == '.' {
+				if row >= 0 && row < n && col >= 0 && col < m && !visited[row][col] && maze[row][col] == '.' {
 					if row == 0 || row == n-1 || col == 0 || col == m-1 {
 						return res
 					}
 
-					res++
 					temp = append(temp, []int{row, col})
 				}
 			}
