651b20a274c614c7473b6339fc251d6ca3591574
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-02 14:46:03 +0700

    34. Find First and Last Position of Element in Sorted Array

---
+++ Leetcode/solutions/medium/34. Find First and Last Position of Element in Sorted Array.go
@@ -0,0 +1,31 @@
+package medium
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Reference: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+func init() {
+	Solutions[34] = func() {
+		fmt.Println("Input: nums = [5,7,7,8,8,10], target = 8")
+		fmt.Println("Output:", searchRange([]int{5, 7, 7, 8, 8, 10}, 8))
+		fmt.Println("Input: nums = [5,7,7,8,8,10], target = 6")
+		fmt.Println("Output:", searchRange([]int{5, 7, 7, 8, 8, 10}, 6))
+		fmt.Println("Input: nums = [], target = 0")
+		fmt.Println("Output:", searchRange([]int{}, 0))
+	}
+}
+
+func searchRange(nums []int, target int) []int {
+	start := sort.SearchInts(nums, target)
+	if start >= len(nums) || nums[start] != target {
+		return []int{-1, -1}
+	}
+
+	end := start
+	for end < len(nums)-1 && nums[end+1] == target {
+		end++
+	}
+	return []int{start, end}
+}
