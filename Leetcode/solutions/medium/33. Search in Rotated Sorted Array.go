6d68f6fb3c980dfe9ce4356bd5f517c5f588ebca
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-02 14:01:13 +0700

    Update 33. Search in Rotated Sorted Array.go

--- Leetcode/solutions/medium/33. Search in Rotated Sorted Array.go
+++ Leetcode/solutions/medium/33. Search in Rotated Sorted Array.go
@@ -15,8 +15,7 @@ func init() {
 }
 
 func search(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for left <= right {
+	for left, right := 0, len(nums)-1; left <= right; {
 		mid := (left + right) / 2
 		if nums[mid] == target {
 			return mid
