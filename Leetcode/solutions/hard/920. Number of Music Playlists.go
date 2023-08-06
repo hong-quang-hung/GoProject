1ff799992ba3a418668b33158f43e6b9c29d2843
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-07 01:26:16 +0700

    920. Number of Music Playlists

--- Leetcode/solutions/hard/920. Number of Music Playlists.go
+++ Leetcode/solutions/hard/920. Number of Music Playlists.go
@@ -6,7 +6,7 @@ import "fmt"
 func init() {
 	Solutions[920] = func() {
 		fmt.Println("Input: n = 3, goal = 3, k = 1")
-		fmt.Println("Output:", numMusicPlaylists(3, 1, 1))
+		fmt.Println("Output:", numMusicPlaylists(3, 3, 1))
 		fmt.Println("Input: n = 2, goal = 3, k = 0")
 		fmt.Println("Output:", numMusicPlaylists(2, 3, 0))
 		fmt.Println("Input: n = 2, goal = 3, k = 1")
@@ -15,5 +15,33 @@ func init() {
 }
 
 func numMusicPlaylists(n int, goal int, k int) int {
-	return 1
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, goal+1)
+		for j := 0; j <= goal; j++ {
+			dp[i][j] = -1
+		}
+	}
+	return numMusicPlaylistsSolved(n, k, dp, n, goal)
+}
+
+func numMusicPlaylistsSolved(n int, k int, dp [][]int, i int, j int) int {
+	if i == 0 && j == 0 {
+		return 1
+	}
+
+	if i == 0 || j == 0 {
+		return 0
+	}
+
+	if dp[i][j] != -1 {
+		return dp[i][j]
+	}
+
+	dp[i][j] = (numMusicPlaylistsSolved(n, k, dp, i-1, j-1) * (n - i + 1)) % mod
+	if i > k {
+		dp[i][j] += (numMusicPlaylistsSolved(n, k, dp, i, j-1) * (i - k)) % mod
+		dp[i][j] %= mod
+	}
+	return dp[i][j]
 }
