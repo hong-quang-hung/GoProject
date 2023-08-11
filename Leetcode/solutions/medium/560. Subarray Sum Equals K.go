b5740db752c3035bc92e29954d122c76b510d27b
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-11 14:12:56 +0700

    560. Subarray Sum Equals K

--- Leetcode/solutions/medium/560. Subarray Sum Equals K.go
+++ Leetcode/solutions/medium/560. Subarray Sum Equals K.go
@@ -8,18 +8,18 @@ func init() {
 		fmt.Println("Input: nums = [1,1,1], k = 2")
 		fmt.Println("Output:", subarraySum([]int{1, 1, 1}, 2))
 		fmt.Println("Input: nums = [1,2,3], k = 3")
-		fmt.Println("Output:", subarraySum([]int{1, 2, 3}, 1))
+		fmt.Println("Output:", subarraySum([]int{1, 2, 3}, 3))
 	}
 }
 
 func subarraySum(nums []int, k int) int {
-	n := len(nums)
-	prefix := make([]int, n)
-	prefix[0] = nums[0]
-	for i := 1; i < n; i++ {
-		prefix[i] = prefix[i-1] + nums[i]
+	m := map[int]int{}
+	m[0] = 1
+	res, sum := 0, 0
+	for _, num := range nums {
+		sum += num
+		res += m[sum-k]
+		m[sum]++
 	}
-
-	fmt.Println(prefix)
-	return 0
+	return res
 }
