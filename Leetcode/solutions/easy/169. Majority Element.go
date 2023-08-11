3540aa78a4a9b4afb3bbc9dce22bf4f1bcd54cc6
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-11 14:49:48 +0700

    169. Majority Element

---
+++ Leetcode/solutions/easy/169. Majority Element.go
@@ -0,0 +1,30 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/majority-element/
+func init() {
+	Solutions[169] = func() {
+		fmt.Println("Input: nums = [3,2,3]")
+		fmt.Println("Output:", majorityElement([]int{3, 2, 3}))
+		fmt.Println("Input: nums = [2,2,1,1,1,2,2]")
+		fmt.Println("Output:", majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+	}
+}
+
+func majorityElement(nums []int) int {
+	res := nums[0]
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			res = nums[i]
+		}
+
+		if res == nums[i] {
+			count++
+		} else {
+			count--
+		}
+	}
+	return res
+}
