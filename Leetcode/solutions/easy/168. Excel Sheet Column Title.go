e6524ce46b687d82ce2cf695d02c87b3056512d9
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-22 09:43:07 +0700

    168. Excel Sheet Column Title

---
+++ Leetcode/solutions/easy/168. Excel Sheet Column Title.go
@@ -0,0 +1,29 @@
+package easy
+
+import (
+	"fmt"
+)
+
+// Reference: https://leetcode.com/problems/excel-sheet-column-title/
+func init() {
+	Solutions[168] = func() {
+		fmt.Println("Input: columnNumber = 1")
+		fmt.Println("Output:", convertToTitle(1))
+		fmt.Println("Input: columnNumber = 28")
+		fmt.Println("Output:", convertToTitle(28))
+		fmt.Println("Input: columnNumber = 52")
+		fmt.Println("Output:", convertToTitle(52))
+		fmt.Println("Input: columnNumber = 701")
+		fmt.Println("Output:", convertToTitle(701))
+	}
+}
+
+func convertToTitle(columnNumber int) string {
+	res := []byte{}
+	for columnNumber >= 1 {
+		columnNumber--
+		res = append([]byte{byte(columnNumber%26 + int('A'))}, res...)
+		columnNumber /= 26
+	}
+	return string(res)
+}
