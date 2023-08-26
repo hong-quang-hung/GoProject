a6a63941b302b4e879559b91be689980cf03dc06
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-08-26 08:09:54 +0700

    68. Text Justification

--- Leetcode/solutions/hard/68. Text Justification.go
+++ Leetcode/solutions/hard/68. Text Justification.go
@@ -2,6 +2,8 @@ package hard
 
 import (
 	"fmt"
+	"math"
+	"strings"
 )
 
 // Reference: https://leetcode.com/problems/text-justification/
@@ -17,5 +19,46 @@ func init() {
 }
 
 func fullJustify(words []string, maxWidth int) []string {
-	return nil
+	res := []string{}
+	for i := 0; i < len(words); i++ {
+		cache := []string{}
+		size := 0
+		j := i
+		for ; j < len(words); j++ {
+			if size+len(words[j]) <= maxWidth {
+				cache = append(cache, words[j])
+				size += len(words[j]) + 1
+			} else {
+				j--
+				break
+			}
+		}
+		newLine := buildString(cache, maxWidth, j == len(words))
+		i = j
+		res = append(res, newLine)
+	}
+	return res
+}
+
+func buildString(words []string, maxWidth int, isLast bool) string {
+	res := ""
+	tempResult := strings.Join(words, " ")
+	if isLast || len(words) == 1 {
+		res = tempResult
+		for i := len(res); i < maxWidth; i++ {
+			res += " "
+		}
+		return res
+	}
+
+	totalSpace := maxWidth - len(tempResult) + len(words) - 1
+	for i := 0; i < len(words); i++ {
+		res += words[i]
+		numSpaces := int(math.Ceil(float64(totalSpace) / float64(len(words)-1-i)))
+		totalSpace -= numSpaces
+		for s := 0; s < numSpaces; s++ {
+			res += " "
+		}
+	}
+	return res
 }
