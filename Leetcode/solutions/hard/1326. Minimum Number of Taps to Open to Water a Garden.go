634f2d4cbe2176ea587c0ff6d7e0bf2b27c33f58
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-09-01 06:36:01 +0700

    1326. Minimum Number of Taps to Open to Water a Garden

--- Leetcode/solutions/hard/1326. Minimum Number of Taps to Open to Water a Garden.go
+++ Leetcode/solutions/hard/1326. Minimum Number of Taps to Open to Water a Garden.go
@@ -2,7 +2,7 @@ package hard
 
 import "fmt"
 
-// Reference: https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
+// Reference: https://leetcode.com/problems/minimum-number-of-res-to-open-to-water-a-garden/
 func init() {
 	Solutions[1326] = func() {
 		fmt.Println("Input: n = 5, ranges = [3,4,1,1,0,0]")
@@ -13,5 +13,24 @@ func init() {
 }
 
 func minTaps(n int, ranges []int) int {
-	return 0
+	maxReach := make([]int, n+1)
+	for i := 0; i < len(ranges); i++ {
+		start := max(0, i-ranges[i])
+		end := min(n, i+ranges[i])
+		maxReach[start] = max(maxReach[start], end)
+	}
+
+	res, currEnd, nextEnd := 0, 0, 0
+	for i := 0; i <= n; i++ {
+		if i > nextEnd {
+			return -1
+		}
+
+		if i > currEnd {
+			res++
+			currEnd = nextEnd
+		}
+		nextEnd = max(nextEnd, maxReach[i])
+	}
+	return res
 }
