c52a720ab5db23905d8d7d89fcce03aad07b2899
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-09-06 15:18:38 +0700

    2483. Minimum Penalty for a Shop

---
+++ Leetcode/solutions/medium/2483. Minimum Penalty for a Shop.go
@@ -0,0 +1,32 @@
+package medium
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/minimum-penalty-for-a-shop/
+func init() {
+	Solutions[2483] = func() {
+		fmt.Println("Input: customers = 'YYNY'")
+		fmt.Println("Output:", bestClosingTime("YYNY"))
+		fmt.Println("Input: customers = 'NNNNN'")
+		fmt.Println("Output:", bestClosingTime("NNNNN"))
+		fmt.Println("Input: customers = 'YYYY'")
+		fmt.Println("Output:", bestClosingTime("YYYY"))
+	}
+}
+
+func bestClosingTime(customers string) int {
+	maxScore, score, bestHour := 0, 0, -1
+	for i := range customers {
+		if customers[i] == 'Y' {
+			score++
+		} else {
+			score--
+		}
+
+		if score > maxScore {
+			maxScore = score
+			bestHour = i
+		}
+	}
+	return bestHour + 1
+}
