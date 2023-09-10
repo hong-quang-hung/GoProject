d873b9d03f723fdc5614d6bbb12e27453039cb9e
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2023-09-10 22:45:44 +0700

    1359. Count All Valid Pickup and Delivery Options

---
+++ Leetcode/solutions/hard/1359. Count All Valid Pickup and Delivery Options.go
@@ -0,0 +1,23 @@
+package hard
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+func init() {
+	Solutions[1359] = func() {
+		fmt.Println("Input: n = 1")
+		fmt.Println("Output:", countOrders(1))
+		fmt.Println("Input: n = 2")
+		fmt.Println("Output:", countOrders(2))
+		fmt.Println("Input: n = 3")
+		fmt.Println("Output:", countOrders(3))
+	}
+}
+
+func countOrders(n int) int {
+	res := 1
+	for i := 2; i <= n; i++ {
+		res = (res * (2*i - 1) * i) % mod
+	}
+	return res
+}
