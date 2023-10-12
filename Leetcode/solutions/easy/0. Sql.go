29e687b69bedc614bb7d567f7b43b4dd7e115c46
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-12 13:56:40 +0700

    1148. Article View

--- Leetcode/solutions/easy/0. Sql.go
+++ Leetcode/solutions/easy/0. Sql.go
@@ -3,6 +3,7 @@ package easy
 func init() {
 	Solutions[584] = Leetcode_SQL
 	Solutions[595] = Leetcode_SQL
+	Solutions[1148] = Leetcode_SQL
 	Solutions[1757] = Leetcode_SQL
 	Solutions[1965] = Leetcode_SQL
 }
---
+++ Leetcode/solutions/easy/1148. Article Views I.sql
@@ -0,0 +1,8 @@
+/* https://leetcode.com/problems/article-views-i/ */
+/* Write your T-SQL query statement below */
+
+SELECT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+ORDER BY id
