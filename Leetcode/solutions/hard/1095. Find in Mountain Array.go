cee3729c2cc6e6e49a1c9a0c5173338c53e1f4f3
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-12 15:58:16 +0700

    1095. Find in Mountain Array

--- Leetcode/solutions/hard/1095. Find in Mountain Array.go
+++ Leetcode/solutions/hard/1095. Find in Mountain Array.go
@@ -1,16 +1,19 @@
 package hard
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Reference: https://leetcode.com/problems/find-in-mountain-array/
 func init() {
 	Solutions[1095] = func() {
-		// fmt.Println("Input: array = [1,2,3,4,5,3,1], target = 3")
-		// fmt.Println("Output:", findInMountainArray(3, MountainArrayConstructor([]int{1, 2, 3, 4, 5, 3, 1})))
-		// fmt.Println("Input: array = [0,1,2,4,2,1], target = 3")
-		// fmt.Println("Output:", findInMountainArray(3, MountainArrayConstructor([]int{0, 1, 2, 4, 2, 1})))
-		fmt.Println("Input: array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82], target = 101")
-		fmt.Println("Output:", findInMountainArray(101, MountainArrayConstructor([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82})))
+		fmt.Println("Input: array = [1,2,3,4,5,3,1], target = 3")
+		fmt.Println("Output:", findInMountainArray(3, MountainArrayConstructor([]int{1, 2, 3, 4, 5, 3, 1})))
+		fmt.Println("Input: array = [0,1,2,4,2,1], target = 3")
+		fmt.Println("Output:", findInMountainArray(3, MountainArrayConstructor([]int{0, 1, 2, 4, 2, 1})))
+		fmt.Println("Input: array = [0,5,3,1], target = 1")
+		fmt.Println("Output:", findInMountainArray(1, MountainArrayConstructor([]int{0, 5, 3, 1})))
 	}
 }
 
@@ -32,24 +35,35 @@ func MountainArrayConstructor(array []int) *MountainArray {
 }
 
 func findInMountainArray(target int, mountainArr *MountainArray) int {
-	left, right := 0, mountainArr.length()-1
-	for left <= right {
-		mid := (left + right) >> 1
-		midVal, leftVal, rightVal := mountainArr.get(mid), mountainArr.get(left), mountainArr.get(right)
-		if leftVal == target {
-			return left
-		}
-
-		fmt.Println(mountainArr.arr[left:right+1], midVal)
-
-		if midVal > target && midVal <= leftVal || midVal < target && midVal > leftVal {
-			left = mid + 1
-		} else if midVal > target && midVal <= rightVal || midVal < target && midVal >= rightVal {
-			left++
-			right = mid - 1
+	n := mountainArr.length()
+	left, right := 0, n-1
+	mid := (right + left) / 2
+	for mountainArr.get(mid-1) >= mountainArr.get(mid) || mountainArr.get(mid) <= mountainArr.get(mid+1) {
+		if mountainArr.get(mid) <= mountainArr.get(mid+1) {
+			left = mid
 		} else {
-			left++
+			right = mid
 		}
+		mid = (right + left) / 2
 	}
-	return -1
+
+	if mountainArr.get(mid) == target {
+		return mid
+	}
+
+	res := sort.Search(mid, func(i int) bool {
+		return mountainArr.get(i) >= target
+	})
+
+	if mountainArr.get(res) != target {
+		res = sort.Search(n-mid-1, func(i int) bool {
+			return mountainArr.get(mid+i+1) <= target
+		})
+		res = mid + res + 1
+		if n == res || mountainArr.get(res) != target {
+			return -1
+		}
+		return res
+	}
+	return res
 }
