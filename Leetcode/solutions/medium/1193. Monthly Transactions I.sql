d4827c1c66df98a77990f11ea4f174a34ad00c63
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-13 17:05:16 +0700

    1193. Monthly Transactions I

--- Leetcode/solutions/medium/1193. Monthly Transactions I.sql
+++ Leetcode/solutions/medium/1193. Monthly Transactions I.sql
@@ -1,2 +1,10 @@
 /* https://leetcode.com/problems/monthly-transactions-i/ */
 /* Write your T-SQL query statement below */
+
+SELECT CONVERT(CHAR(7), trans_date , 126) AS month, country
+    , COUNT(1) AS trans_count
+    , SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count
+    , SUM(amount) AS trans_total_amount
+    , SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY country, CONVERT(CHAR(7), trans_date, 126)
