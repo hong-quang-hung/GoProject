a7d9ed8f9416590046dda895e1ecf8e59af80c24
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-16 13:22:23 +0700

    2356. Number of Unique Subjects Taught by Each Teacher

---
+++ Leetcode/solutions/easy/2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,6 @@
+/* https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/ */
+/* Write your T-SQL query statement below */
+
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id
