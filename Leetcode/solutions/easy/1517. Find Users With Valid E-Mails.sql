079e25a0c85c55b4e3ec03e872651dff471cedce
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-16 17:08:48 +0700

    1517. Find Users With Valid E-Mails

---
+++ Leetcode/solutions/easy/1517. Find Users With Valid E-Mails.sql
@@ -0,0 +1,6 @@
+/* https://leetcode.com/problems/find-users-with-valid-e-mails/description/ */
+/* Write your T-SQL query statement below */
+
+SELECT user_id, name, mail
+FROM Users
+WHERE mail LIKE '[a-zA-Z]%@leetcode.com' AND LEFT(mail, LEN(mail) - 13) NOT LIKE '%[^a-zA-Z_0-9_.-]%'
