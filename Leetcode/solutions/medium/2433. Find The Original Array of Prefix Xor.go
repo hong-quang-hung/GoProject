c59d274ee0cd8f66d425e981f3edcc2fb5063b0f
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-10-31 08:34:02 +0700

    2433. Find The Original Array of Prefix Xor

---
+++ Leetcode/solutions/medium/2433. Find The Original Array of Prefix Xor.go
@@ -0,0 +1,23 @@
+package medium
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+func init() {
+	Solutions[2433] = func() {
+		fmt.Println("Input: pref = [5,2,0,3,1]")
+		fmt.Println("Output:", findArray([]int{5, 2, 0, 3, 1}))
+		fmt.Println("Input: pref = [13]")
+		fmt.Println("Output:", findArray([]int{13}))
+	}
+}
+
+func findArray(pref []int) []int {
+	prefix := pref[0]
+	for i := 1; i < len(pref); i++ {
+		temp := pref[i]
+		pref[i] = prefix ^ pref[i]
+		prefix = temp
+	}
+	return pref
+}
