e81afb1b31b87a4c9c02780b15949f697940982e
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-11-01 13:12:41 +0700

    1484. Group Sold Products By The Date

---
+++ Leetcode/solutions/easy/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/second-highest-salary/
+# Write your MySQL query statement below
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(
+        DISTINCT product
+        ORDER BY
+            product SEPARATOR ','
+    ) AS products
+FROM
+    Activities
+GROUP BY
+    sell_date
+ORDER BY
+    sell_date
