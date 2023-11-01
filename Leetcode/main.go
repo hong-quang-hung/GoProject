7d65ce9876f2a0ebb0f4562ff8efd1f3b5d90845
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-11-01 10:05:32 +0700

    501. Find Mode in Binary Search Tree

--- Leetcode/main.go
+++ Leetcode/main.go
@@ -26,7 +26,7 @@ func init() {
 	problemDebug = 2050
 	problemDebug = 1425
 	problemDebug = 823
-	problemDebug = 57
+	problemDebug = 501
 
 	problemTotal = 2920
 }
---
+++ Leetcode/solutions/easy/501. Find Mode in Binary Search Tree.go
@@ -0,0 +1,46 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/find-mode-in-binary-search-tree/
+func init() {
+	Solutions[501] = func() {
+		fmt.Println("Input: root = [1,null,2,2]")
+		fmt.Println("Output:", findMode(S2TreeNode("[1,null,2,2]")))
+		fmt.Println("Input: root = [0]")
+		fmt.Println("Output:", findMode(S2TreeNode("[0]")))
+	}
+}
+
+func findModeInorder(root *TreeNode, maxFreq, currVal, freq *int, sol *[]int) {
+	if root.Left != nil {
+		findModeInorder(root.Left, maxFreq, currVal, freq, sol)
+	}
+
+	if root.Val == (*currVal) || (*freq) == 0 {
+		(*freq)++
+	} else if root.Val != (*currVal) {
+		(*freq) = 1
+	}
+
+	(*currVal) = root.Val
+	if (*freq) > (*maxFreq) {
+		(*maxFreq) = (*freq)
+		(*sol) = []int{root.Val}
+	} else if (*freq) == (*maxFreq) {
+		(*sol) = append((*sol), root.Val)
+	}
+
+	if root.Right != nil {
+		findModeInorder(root.Right, maxFreq, currVal, freq, sol)
+	}
+}
+
+func findMode(root *TreeNode) []int {
+	maxFreq := 0
+	freq := 0
+	currVal := 0
+	res := []int{}
+	findModeInorder(root, &maxFreq, &currVal, &freq, &res)
+	return res
+}
