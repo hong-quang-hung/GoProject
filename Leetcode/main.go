4a46f19031086b7e8d2c8f9120a5f8c6388b1a59
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-11-07 14:40:18 +0700

    82. Remove Duplicates from Sorted List II

--- Leetcode/main.go
+++ Leetcode/main.go
@@ -25,7 +25,7 @@ func init() {
 	problemDebug = 1458
 	problemDebug = 2050
 	problemDebug = 1425
-	problemDebug = 224
+	problemDebug = 82
 
 	problemTotal = 2926
 }
---
+++ Leetcode/solutions/medium/82. Remove Duplicates from Sorted List II.go
@@ -0,0 +1,32 @@
+package medium
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+func init() {
+	Solutions[82] = func() {
+		fmt.Println("Input: head = [1,2,3,3,4,4,5]")
+		fmt.Println("Output:", SListNode(deleteDuplicates(S2ListNode("[1,2,3,3,4,4,5]"))))
+		fmt.Println("Input: head = [1,1,1,2,3]")
+		fmt.Println("Output:", SListNode(deleteDuplicates(S2ListNode("[1,1,1,2,3]"))))
+	}
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	temp := head
+	for temp.Next != nil && temp.Val == temp.Next.Val {
+		temp = temp.Next
+	}
+
+	if head != temp && head.Val == temp.Val {
+		head = deleteDuplicates(temp.Next)
+	} else {
+		head = temp
+		head.Next = deleteDuplicates(head.Next)
+	}
+	return head
+}
