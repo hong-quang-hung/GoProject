01c07b34e1fee5060dabd5660d28195af8ea2a02
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-11-07 15:32:03 +0700

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.go

--- Leetcode/solutions/medium/105. Construct Binary Tree from Preorder and Inorder Traversal.go
+++ Leetcode/solutions/medium/105. Construct Binary Tree from Preorder and Inorder Traversal.go
@@ -5,36 +5,30 @@ import "fmt"
 // Reference: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 func init() {
 	Solutions[105] = func() {
-		// fmt.Println("Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]")
-		// fmt.Println("Output:", STreeNode(buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})))
-		// fmt.Println("Input: preorder = [-1], inorder = [-1]")
-		// fmt.Println("Output:", STreeNode(buildTree([]int{-1}, []int{-1})))
+		fmt.Println("Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]")
+		fmt.Println("Output:", STreeNode(buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})))
+		fmt.Println("Input: preorder = [-1], inorder = [-1]")
+		fmt.Println("Output:", STreeNode(buildTree([]int{-1}, []int{-1})))
 		fmt.Println("Input: preorder = [1,4,2,3], inorder = [1,2,3,4]")
 		fmt.Println("Output:", STreeNode(buildTree([]int{1, 4, 2, 3}, []int{1, 2, 3, 4})))
 	}
 }
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
-	return buildTreeSolved(preorder, inorder, 0, len(preorder)-1, 0, len(inorder)-1)
-}
-
-func buildTreeSolved(preorder []int, inorder []int, p1 int, p2 int, i1 int, i2 int) *TreeNode {
-	mid, rootVal := i1, preorder[p1]
-	for mid <= i2 && inorder[mid] != rootVal {
-		mid++
+	n := len(inorder)
+	if n == 0 {
+		return nil
 	}
 
-	root := &TreeNode{Val: rootVal}
-	if mid-i1 == 1 {
-		root.Left = &TreeNode{Val: inorder[i1]}
-	} else if mid-i1 > 1 {
-		root.Left = buildTreeSolved(preorder, inorder, p1+1, p1+mid-i1, i1, mid-1)
+	pv := preorder[0]
+	pi := 0
+	for pi < n && inorder[pi] != pv {
+		pi++
 	}
 
-	if i2-mid == 1 {
-		root.Right = &TreeNode{Val: preorder[p2]}
-	} else if i2-mid > 1 {
-		root.Right = buildTreeSolved(preorder, inorder, p1+mid-i1+1, p2, mid+1, i2)
-	}
-	return root
+	res := new(TreeNode)
+	res.Val = pv
+	res.Left = buildTree(preorder[1:], inorder[:pi])
+	res.Right = buildTree(preorder[1+pi:], inorder[pi+1:])
+	return res
 }
