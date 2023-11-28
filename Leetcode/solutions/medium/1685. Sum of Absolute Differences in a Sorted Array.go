022f6975a14ed17bb708c0be8e297d47c32a4a9f
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-11-28 17:08:36 +0700

    .

--- Leetcode/solutions/medium/1685. Sum of Absolute Differences in a Sorted Array.go
+++ Leetcode/solutions/medium/1685. Sum of Absolute Differences in a Sorted Array.go
@@ -16,24 +16,15 @@ func getSumAbsoluteDifferences(nums []int) []int {
 	n := len(nums)
 	res := make([]int, n)
 	prefix := make([]int, n)
-	prefix[0] = nums[0]
+	prefix[0] = 0
 	for i := 1; i < n; i++ {
-		prefix[i] = prefix[i-1] + nums[i]
+		prefix[i] = prefix[i-1] + nums[i] - nums[i-1]
 	}
+
 	fmt.Println(prefix)
 
-	prefixR := make([]int, n)
-	prefixR[n-1] = nums[n-1]
-	for i := n - 2; i >= 0; i-- {
-		prefixR[i] = prefixR[i+1] + nums[i]
-	}
-	fmt.Println(prefixR)
-
 	for i := 0; i < n-1; i++ {
-		for j := i + 1; j < n; j++ {
-			absolute := abs(nums[i] - nums[j])
-			res[i], res[j] = res[i]+absolute, res[j]+absolute
-		}
+
 	}
 	return res
 }
