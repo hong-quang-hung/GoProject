6c39158dd0d9d4050df5752686809066e5aeda53
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-12-05 08:08:44 +0700

    1688. Count of Matches in Tournament

---
+++ Leetcode/solutions/easy/1688. Count of Matches in Tournament.go
@@ -0,0 +1,17 @@
+package easy
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/count-of-matches-in-tournament/
+func init() {
+	Solutions[1688] = func() {
+		fmt.Println("Input: n = 7")
+		fmt.Println("Output:", numberOfMatches(7))
+		fmt.Println("Input: n = 14")
+		fmt.Println("Output:", numberOfMatches(14))
+	}
+}
+
+func numberOfMatches(n int) int {
+	return n - 1
+}
