313ce1dec3331390b64fd4c431883a661d138261
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2023-12-12 10:06:15 +0700

    201. Bitwise AND of Numbers Range

--- Leetcode/solutions/medium/201. Bitwise AND of Numbers Range.go
+++ Leetcode/solutions/medium/201. Bitwise AND of Numbers Range.go
@@ -12,11 +12,26 @@ func init() {
 		fmt.Println("Output:", rangeBitwiseAnd(5, 7))
 		fmt.Println("Input: left = 1, right = 2147483647")
 		fmt.Println("Output:", rangeBitwiseAnd(1, 2147483647))
+		fmt.Println("Input: left = 5, right = 5")
+		fmt.Println("Output:", rangeBitwiseAnd(5, 5))
 	}
 }
 
 func rangeBitwiseAnd(left int, right int) int {
-	fmt.Println(strconv.FormatInt(int64(left), 2))
-	fmt.Println(strconv.FormatInt(int64(right), 2))
-	return 1
+	leftStr := strconv.FormatInt(int64(left), 2)
+	rightStr := strconv.FormatInt(int64(right), 2)
+
+	if len(leftStr) != len(rightStr) {
+		return 0
+	}
+
+	res := 0
+	for i := 0; i < len(leftStr); i++ {
+		if leftStr[i] == '1' && rightStr[i] == '1' {
+			res += 1 << (len(leftStr) - i - 1)
+		} else if leftStr[i] != rightStr[i] {
+			break
+		}
+	}
+	return res
 }
