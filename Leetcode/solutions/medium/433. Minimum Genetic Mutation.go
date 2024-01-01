371d9c944195dc9c6fd3e2d45c54d4fc427ab2c6
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2024-01-02 01:56:29 +0700

    433. Minimum Genetic Mutation.go

--- Leetcode/solutions/medium/433. Minimum Genetic Mutation.go
+++ Leetcode/solutions/medium/433. Minimum Genetic Mutation.go
@@ -13,5 +13,38 @@ func init() {
 }
 
 func minMutation(startGene string, endGene string, bank []string) int {
-	return 0
+	b := make(map[string]interface{})
+	for _, gen := range bank {
+		b[gen] = struct{}{}
+	}
+
+	res := 0
+	choices := []byte{'A', 'C', 'G', 'T'}
+	queue := []string{startGene}
+	visited := make(map[string]interface{})
+	visited[startGene] = struct{}{}
+	for len(queue) > 0 {
+		nextGens := []string{}
+		for _, q := range queue {
+			if q == endGene {
+				return res
+			}
+
+			for _, choice := range choices {
+				for i := 0; i < 8; i++ {
+					nextGen := q[:i] + string(choice) + q[i+1:]
+					if _, isVisited := visited[nextGen]; !isVisited {
+						if _, ok := b[nextGen]; ok {
+							visited[nextGen] = struct{}{}
+							nextGens = append(nextGens, nextGen)
+						}
+					}
+				}
+			}
+		}
+		queue = nextGens
+		nextGens = nil
+		res++
+	}
+	return -1
 }
