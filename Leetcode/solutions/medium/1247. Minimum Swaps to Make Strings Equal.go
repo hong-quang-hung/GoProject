90a33303f17adca95fa322de6f1fe69474304532
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2024-01-16 06:42:10 +0700

    Create 1247. Minimum Swaps to Make Strings Equal.go

---
+++ Leetcode/solutions/medium/1247. Minimum Swaps to Make Strings Equal.go
@@ -0,0 +1,29 @@
+package medium
+
+import "fmt"
+
+// Reference: https://leetcode.com/problems/minimum-swaps-to-make-strings-equal/
+func init() {
+	Solutions[1247] = func() {
+		fmt.Println(`Input: s1 = "xx", s2 = "yy""`)
+		fmt.Println(`Output:`, minimumSwap("xx", "yy"))
+		fmt.Println(`Input: s1 = "xy", s2 = "yx""`)
+		fmt.Println(`Output:`, minimumSwap("xy", "yx"))
+		fmt.Println(`Input: s1 = "xx", s2 = "yx""`)
+		fmt.Println(`Output:`, minimumSwap("xx", "yx"))
+	}
+}
+
+func minimumSwap(s1 string, s2 string) int {
+	m1 := make([]int, 2)
+	for i := range s1 {
+		m1[int(s1[i]-'x')]++
+	}
+
+	fmt.Println(m1)
+
+	for i := range s2 {
+		_ = i
+	}
+	return 0
+}
