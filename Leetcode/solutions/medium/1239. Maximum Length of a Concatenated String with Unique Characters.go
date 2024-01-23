d7b42afc1cf1d05960145acea34f95e89bc2ea97
HunghqFast <55377132+hong-quang-hung@users.noreply.github.com>
2024-01-24 06:27:54 +0700

    1239. Maximum Length of a Concatenated String with Unique Characters

--- Leetcode/solutions/medium/1239. Maximum Length of a Concatenated String with Unique Characters.go
+++ Leetcode/solutions/medium/1239. Maximum Length of a Concatenated String with Unique Characters.go
@@ -15,5 +15,30 @@ func init() {
 }
 
 func maxLength(arr []string) int {
-	return 0
+	unique := func(str string) int {
+		var m [26]int
+		for _, ch := range str {
+			m[ch-'a'] += 1
+			if m[ch-'a'] > 1 {
+				return 0
+			}
+		}
+
+		count := 0
+		for _, val := range m {
+			if val == 1 {
+				count += 1
+			}
+		}
+		return count
+	}
+
+	var f func(index int, arr []string, str string) int
+	f = func(index int, arr []string, str string) int {
+		if index == len(arr) {
+			return unique(str)
+		}
+		return max(f(index+1, arr, str+arr[index]), f(index+1, arr, str))
+	}
+	return f(0, arr, "")
 }
