611327dab58d2149956b8c37e3011a0a5bfee114
Hong Quang Hung <55377132+hong-quang-hung@users.noreply.github.com>
2024-02-23 08:10:26 +0700

    997. Find the Town Judge

--- Leetcode/solutions/easy/997. Find the Town Judge.go
+++ Leetcode/solutions/easy/997. Find the Town Judge.go
@@ -15,5 +15,17 @@ func init() {
 }
 
 func findJudge(n int, trust [][]int) int {
+	trusted := make([]int, n+1)
+	trusts := make([]int, n+1)
+	for i := range trust {
+		trusted[trust[i][1]]++
+		trusts[trust[i][0]]++
+	}
+
+	for i := 1; i <= n; i++ {
+		if trusted[i] == n-1 && trusts[i] == 0 {
+			return i
+		}
+	}
 	return -1
 }
